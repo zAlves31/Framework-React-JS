d7639b862510b3adc01efcd653ee3859d1294cbb
??? <>
2023-10-16 10:56:19 -0300

    Finalizado

--- imc-javascript/js/script.js
+++ imc-javascript/js/script.js
@@ -1,45 +1,93 @@
-function calcular() {
-    event.preventDefault();
+const arrPessoas = []; //array vazio
 
-    //pegar os dados do form
-    const nome = document.getElementById('nome').value;
-    let altura = parseFloat(document.getElementById('altura').value);
-    let peso = parseFloat(document.getElementById('peso').value);
+function calcular(e) {
+  e.preventDefault(); //capturar o evento de submit do formulário (para)
 
-    if (isNaN(altura) || isNaN(peso) || nome.trim() == "") {
-        alert("E necessario preencher os numeros corretamente");
-        return;
-    }
+  // pegar os dados do form
+  const nome = document.getElementById("nome").value.trim();
+  const altura = parseFloat(document.getElementById("altura").value);
+  const peso = parseFloat(document.getElementById("peso").value);
 
-    const imc = calcularImc(altura, peso)
+  if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
+    alert("É necessário preecher os números corretamente");
+    return;
+  }
 
-    console.log(nome);
-    console.log(altura);
-    console.log(peso);
-    console.log(imc);
-    console.log(situacao);
-    //validar se estao prenchidos
+  // calcular o imc
+  const imc = calcularImc(altura, peso);
+  // gera o texto da situação
+  const txtSituacao = retornaSituacao(imc);
 
+  // gera o objeto com os dados da pessoa
+  const pessoa = { nome, altura, peso, imc, situacao: txtSituacao }; //ojeto literal
+
+  //adiciona a pessoa na lista
+  arrPessoas.push(pessoa);
+
+  // listar as pessoas
+  listarPessoas();
 }
 
+// return peso / altura ** 2;
+//     return peso / (altura * altura);
 function calcularImc(altura, peso) {
-    return peso / Math.pow(altura, 2);
-    //return peso/altura ** 2;
-    //return peso / (altura * altura);
+  return peso / Math.pow(altura, 2);
 }
 
-function retornarSituacao(imc) {
-    //validar o imc
-    if (imc <= 18.5) {
-        return "Magreza Severa";
-    }else if(imc <= 24.99){
-        return "Peso Normal";
-    }else if(imc <= 29.99){
-        return "Acima do peso";
-    }else if (imc <= 34.99){
-        return "Obesidade I";
-    }else if (imc <= 39.99){
-        return "Obesidade II";
-    }
-    return 'situacao';
+/*
+    Resultado	        Situação
+    Menor que 18.5      Magreza Severa
+    Entre 18.5 e 24.99	Peso normal
+    Entre 25 e 29.99	Acima do peso
+    Entre 30 e 34.99	Obesidade I
+    Entre 35 e 39.99	Obesidade II (severa)
+    Acima de 40	        Cuidado!!! else
+*/
+function retornaSituacao(imc) {
+  if (imc < 18.5) {
+    return "Magreza Severa!";
+  } else if (imc <= 24.99) {
+    return "Peso normal";
+  } else if (imc <= 29.99) {
+    return "Acima do peso";
+  } else if (imc <= 34.99) {
+    return "Obesidade I";
+  } else if (imc <= 39.99) {
+    return "Obesidade II";
+  } else {
+    return "Cuidado!";
+  }
+}
+
+// lista as pessoas na tabela
+function listarPessoas() {
+//   let textoQualquer = [];
+
+//   textoQualquer.push("<p>EDUARDO</p>"); //0
+//   textoQualquer.push("<p>JONAS</p>"); //1
+//   textoQualquer.push("<p>CARLOS</p>"); //2
+
+//   let temp = "";
+
+//   textoQualquer.forEach((e) => {
+//     temp += e;
+//   });
+
+//   document.getElementById("teste").innerHTML = temp;
+
+  let template = "";//string vazia
+
+  arrPessoas.forEach((p) => {//preenche ou acumula valores na sttring
+    template += `
+        <tr>
+            <td>${p.nome}</td>
+            <td>${p.altura}</td>
+            <td>${p.peso}</td>
+            <td>${p.imc}</td>
+            <td>${p.situacao}</td>
+        </tr>`;
+  });
+
+//injeta a string completa no html
+  document.getElementById('cadastro').innerHTML = template;
 }
