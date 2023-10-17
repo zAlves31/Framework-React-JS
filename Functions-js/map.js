5dea4888fbf4ebb65c4898aca7f30dd969cd3a44
zAlves31 <alvesbautistajoaovictor@gmail.com>
2023-10-17 09:05:49 -0300

    Functions

---
+++ Functions-js/map.js
@@ -0,0 +1,31 @@
+const mumeros = [1, 2, 5, 10, 300];
+
+const mesa = ["Coral", "Anna", "Demetrio", "Vinicius", "Lacerda", "Evelyn", "Luiz",]
+
+//calback functions
+// mesa.forEach(
+//     function Carlos(cadaPessoa) {
+//         console.log('Bom dia' + cadaPessoa);
+//     }
+// );
+
+
+//funcao anonima
+// mesa.forEach(
+//     function (cadaPessoa) {
+//         console.log( 'Bom dia ' + cadaPessoa );
+//     }
+// );
+
+//arrow functions
+mesa.forEach( (cadaPessoa) => {
+        console.log( 'Bom dia ' + cadaPessoa );
+    }
+);
+
+
+// const dobro = function(x) {
+//     return x * 356;
+// }
+
+// console.log( dobro(874) );
