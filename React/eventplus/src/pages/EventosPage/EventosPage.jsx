0501733df4ec465a53e5508ed85c663b809ed49b
zAlves31 <alvesbautistajoaovictor@gmail.com>
2023-10-31 08:17:13 -0300

    React

--- React/eventplus/src/pages/EventosPage/EventosPage.jsx
+++ React/eventplus/src/pages/EventosPage/EventosPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoginPage = () => {
     return (
         <div>
-            <h1>Pagina Login</h1>
+            <h1>Eventos Page</h1>
         </div>
     );
 };
--- React/eventplus/src/pages/TipoEventos/TipoEventos.jsx
+++ React/eventplus/src/pages/TipoEventos/TipoEventos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProdutoPage = () => {
     return (
         <div>
-            <h1>Pagina Produto</h1>
+            <h1>Pagina Tipo Eventos</h1>
         </div>
     );
 };
